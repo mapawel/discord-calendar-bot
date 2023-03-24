de478f84b9a59e2517d67f6411bd6b3a593a7827
mapawel <pawelma@gmail.com>
2023-03-24 21:18:02 +0100

    primaryKey in DB as discordId

--- src/roles/entity/Role.entity.ts
+++ src/roles/entity/Role.entity.ts
@@ -1,7 +1,8 @@
-import { Table, Column, Model, Default, Unique } from 'sequelize-typescript';
+import { Table, Column, Model, PrimaryKey, Unique } from 'sequelize-typescript';
 
 @Table
 export class Role extends Model {
+  @PrimaryKey
   @Unique({ name: 'id', msg: 'Role ID must be unique' })
   @Column
   roleId: string;
--- src/users/entity/User.entity.ts
+++ src/users/entity/User.entity.ts
@@ -1,7 +1,15 @@
-import { Table, Column, Model, Default, Unique } from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  Default,
+  Unique,
+  PrimaryKey,
+} from 'sequelize-typescript';
 
 @Table
 export class User extends Model {
+  @PrimaryKey
   @Unique({ name: 'discordId', msg: 'Discord ID must be unique' })
   @Column
   discordId: string;
